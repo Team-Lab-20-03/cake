7d82a17fc90a76d200f1921c3700e7c6439fc97b
Yohey Oomori <link.yohey@gmail.com>
2020-05-02 05:33:13 +0000

    fix boolean default false

--- db/migrate/20200502025752_change_default_of_status.rb
+++ db/migrate/20200502025752_change_default_of_status.rb
@@ -1,11 +1,11 @@
 class ChangeDefaultOfStatus < ActiveRecord::Migration[5.2]
   def up
-  	change_column :customers,        :is_active, 		  :boolean, default: true, null: false
+  	change_column :customers,        :is_active, 		  :boolean, default: false, null: false
   	change_column :ordered_products, :create_status,  :integer, default: 0,    null: false
   	change_column :orders,					 :ordered_status, :integer, default: 0,    null: false
-  	change_column :orders,					 :is_credit,		  :boolean, default: true, null: false
-  	change_column :products,				 :is_active, 			:boolean, default: true, null: false
-  	change_column :genres, 					 :is_active,      :boolean, default: true, null: false
+  	change_column :orders,					 :is_credit,		  :boolean, default: false, null: false
+  	change_column :products,				 :is_active, 			:boolean, default: false, null: false
+  	change_column :genres, 					 :is_active,      :boolean, default: false, null: false
   end
   def down
   	change_column :customers,				 :is_active,		  :boolean
