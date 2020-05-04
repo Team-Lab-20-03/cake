43b9c5d098cd6457eb9fb577f7628818c44d37a5
Yohey Oomori <link.yohey@gmail.com>
2020-05-04 10:42:58 +0900

    初期追加、indexでは遷移減関係なくOrder.all表示状態

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,2 +1,24 @@
 class Admins::OrdersController < ApplicationController
+
+	# 注文履歴一覧
+	def index
+		@orders = Order.all
+	end
+
+	# 注文履歴詳細
+	def show
+		@order = Order.find(params[:id])
+		@ordered_products = @order.ordered_products
+	end
+
+	def update
+		@order = Order.find(params[:id])
+		@order.update(order_params)
+		redirect_to admins_order_path(@order)
+	end
+
+	private
+	def order_params
+		params.require(:order).permit(:is_active)
+	end
 end
