bf2685967a07847ab7290e2e7100145c4c2d0c13
tom0221 <tomuto0221@gmail.com>
2020-05-05 08:35:39 +0000

    退会処理までOKだよん

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,32 +2,36 @@ class CustomersController < ApplicationController
 
 	before_action :authenticate_customer!
 
-def show
-
-	@customer = Customer.find(current_customer.id)
-
-end
-
-def edit
-
-	@customer = Customer.find(current_customer.id)
-
-end
-
-def update
-
-	@customer = Customer.find(current_customer.id)
-    @customer.update
-    redirect_to customers_path
-end
-
-def bye_confirm
-
-	@customer = Customer.find(current_customer.id)
-    if @customer.update!(admittion_status: "bye_confirm")
-
-      redirect_to customers_sign_out_path
+  def show
+	  @customer = Customer.find(params[:id])
   end
-end
+
+  def edit
+	  @customer = Customer.find(params[:id])
+
+  end
+
+  def update
+	  @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to customers_path
+  end
+
+  def bye_confirm
+	@customer = Customer.find(params[:id])
+    # @current_customer.update(params[:id])
+    # redirect_to root_path
+  end
+
+  def bye_bye
+  	@customer = Customer.find(current_customer.id)
+  	@customer.update(is_active: '退会済み')
+    redirect_to root_path
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:is_active, :email, :last_name, :first_name, :kana_last_name, :kana_first_name, :phone_number, :main_zip_code, :main_address )
+  end
 
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,4 +9,36 @@ class Customer < ApplicationRecord
   has_many :orders, dependent: :destroy
 
   enum is_active: {有効: true, 退会済み: false}
+
+
+
+
+class Customer < ActiveRecord::Base
+  composed_of :fullname,
+       :class_name => "FullName",
+       :mapping =>[
+        [ :last_name, :family_name ],
+        [ :first_name, :given_name ]
+       ]
+
+  composed_of :fullkananame,
+       :class_name => "FullName",
+       :mapping =>[
+        [ :last_name, :family_name ],
+        [ :first_name, :given_name ]]
+end
+
+class FullName
+  attr_reader :family_name, :given_name
+
+  def initialize(family_name, given_name)
+    @family_name = family_name
+    @given_name = given_name
+  end
+
+  def to_s
+    [@family_name,@given_name].compact.join("")
+  end
+end
+
 end
