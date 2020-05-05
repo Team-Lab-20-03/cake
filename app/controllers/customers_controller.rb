f0bbe3ebf492c3d612202e938b56a9d1b6a4cde1
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-05-05 09:10:39 +0000

    修正

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,44 +2,36 @@ class CustomersController < ApplicationController
 
 	before_action :authenticate_customer!
 
-  def edit
+  def show
+    @customer = Customer.find(params[:id])
+  end
 
+  def edit
     @customer = Customer.find(params[:id])
 
   end
 
   def update
-
     @customer = Customer.find(params[:id])
-    if @customer.update(customer_params)
-      redirect_to customer_path(@customer.id)
-    else
-      render :edit
-    end
-  end
-
-  def show
-
-    @customer = Customer.find(params[:id])
-
+    @customer.update(customer_params)
+    redirect_to customers_path
   end
 
   def bye_confirm
+  @customer = Customer.find(params[:id])
+    # @current_customer.update(params[:id])
+    # redirect_to root_path
   end
 
   def bye_bye
-    @customer = Customer.find(params[:id])
-    if @customer.update!(admittion_status: "bye_bye")
-      redirect_to customers_sign_out_path
-    end
+    @customer = Customer.find(current_customer.id)
+    @customer.update(is_active: '退会済み')
+    redirect_to root_path
   end
 
   private
-
   def customer_params
-
-    params.require(:customer).permit(:first_name, :last_name, :kana_first_name, :kana_last_name, :email, :main_zip_code, :main_address, :phone_number, :is_active)
-
+    params.require(:customer).permit(:is_active, :email, :last_name, :first_name, :kana_last_name, :kana_first_name, :phone_number, :main_zip_code, :main_address )
   end
 
 end
