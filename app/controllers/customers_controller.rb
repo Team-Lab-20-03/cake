c9db6843d7c07e1d7fcf34a38688130d563cceb2
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-05-05 06:09:40 +0000

    登録情報エラー解消

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,32 +2,44 @@ class CustomersController < ApplicationController
 
 	before_action :authenticate_customer!
 
-def show
+  def edit
 
-	@customer = Customer.find(current_customer.id)
+    @customer = Customer.find(params[:id])
 
-end
-
-def edit
-
-	@customer = Customer.find(current_customer.id)
-
-end
-
-def update
-
-	@customer = Customer.find(current_customer.id)
-    @customer.update
-    redirect_to customers_path
-end
-
-def bye_confirm
-
-	@customer = Customer.find(current_customer.id)
-    if @customer.update!(admittion_status: "bye_confirm")
-
-      redirect_to customers_sign_out_path
   end
-end
+
+  def update
+
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer.id)
+    else
+      render :edit
+    end
+  end
+
+  def show
+
+    @customer = Customer.find(params[:id])
+
+  end
+
+  def bye_confirm
+  end
+
+  def bye_bye
+    @customer = Customer.find(params[:id])
+    if @customer.update!(admittion_status: "bye_bye")
+      redirect_to customers_sign_out_path
+    end
+  end
+
+  private
+
+  def customer_params
+
+    params.require(:customer).permit(:first_name, :last_name, :kana_first_name, :kana_last_name, :email, :main_zip_code, :main_address, :phone_number, :is_active)
+
+  end
 
 end
