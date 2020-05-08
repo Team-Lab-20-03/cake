81b53d2d2e2755a6b2b05f0c7d7f6610e595216f
yu-san51 <nekonyan.yuki51@gmail.com>
2020-05-08 06:55:12 +0000

    cartitem update

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -8,14 +8,6 @@ class CartItem < ApplicationRecord
 		self.product.tax_price * self.quantity
 	end
 
-	def cart_sum(user)
-		total = 0
-		user.cart_items.each do |cart_product|
-			total += cart_product.product.tax_price
-		end
-		total
-	end
-
 
 
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -13,6 +13,15 @@ class Customer < ApplicationRecord
   # is_activeが有効の場合、sessionsコントローラへtrueを返す
  def active_for_authentication?
  	super && (self.is_active == '有効')
-end
+ end
+  # カート内アイテム合計
+ def cart_sum
+	total = 0
+	cart_items.each do |cart_product|
+		total += cart_product.product.tax_price * cart_product.quantity
+	end
+	total
+ end
+
 
 end
