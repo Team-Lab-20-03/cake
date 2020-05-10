d8781107322ceb69f88da262831b6a80076d61af
tom0221 <tomuto0221@gmail.com>
2020-05-10 05:24:42 +0000

    ec-topに商品ランダム機能追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def top
-  	@products = Product.page(params[:page]).reverse_order #変えてる
-  	@genres = Genre.all
+  	@products = Product.page(params[:page]).reverse_order #ページネーション変えてる
+  	@random = Product.order("RANDOM()").limit(4)#ランダム機能追加
+  	@genres = Genre.all #Topにもジャンル検索項目を追加する。
+
   end
 
 
