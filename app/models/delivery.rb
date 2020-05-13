9e168f130f96906f06121ea6a4e81e4db452a4cb
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-05-13 06:18:00 +0000

    修正

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -6,11 +6,11 @@ class Delivery < ApplicationRecord
 		self.zip_code + self.address + self.destination_name
 	end
 
-	住所バリデーション
-    VALID_ZIP_CODE = /\A\d{3}[-]\d{4}\z/
-    validates :zip_code, presence: true, format: { with: VALID_ZIP_CODE, message: "はハイフンあり7桁で入力してください。" }
-    validates :address, presence: true
-    validates :destination_name, presence: true
+	# 住所バリデーション
+ #    VALID_ZIP_CODE = /\A\d{3}[-]\d{4}\z/
+ #    validates :zip_code, presence: true, format: { with: VALID_ZIP_CODE, message: "はハイフンあり7桁で入力してください。" }
+ #    validates :address, presence: true
+ #    validates :destination_name, presence: true
 
 
 end
